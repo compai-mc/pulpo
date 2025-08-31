f95f1e7d01d01fd37b0f1ff89c9b1115dec012dd
PepeRedondoRubio <peperedondorubio@gmail.com>
2025-08-31 10:30:18 +0200

    feat: actualizar versión a 1.9.3 en setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pulpo",
-    version="1.9.2",
+    version="1.9.3",
     packages=find_packages(),
     install_requires=[
         "requests", 
