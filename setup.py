70fadaa46cea38a8b5d053d1a4547e75d935193b
PepeRedondoRubio <peperedondorubio@gmail.com>
2025-09-01 09:19:48 +0200

    Nueva version y _on_kafka

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pulpo",
-    version="1.9.13",
+    version="1.9.14",
     packages=find_packages(),
     install_requires=[
         "requests", 
