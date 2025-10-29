0d72fa48f4954066181fb0e9afbe18d0a99e45b4
PepeRedondoRubio <peperedondorubio@gmail.com>
2025-10-29 16:56:58 +0100

    refactor: cambiar ERPProxy a ERPProxySincrono para la creación de documentos de propuesta; actualizar versión a 4.4.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pulpo",
-    version="4.4.5",
+    version="4.4.6",
     packages=find_packages(),
     install_requires=[
         "requests", 
