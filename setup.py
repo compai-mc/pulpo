0314f9703d26c10b881c9f983a07cca218a0fe63
PepeRedondoRubio <peperedondorubio@gmail.com>
2025-10-30 12:45:30 +0100

    fix: correct version number in setup.py from 4.4.8s to 4.4.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pulpo",
-    version="4.4.8s",
+    version="4.4.8",
     packages=find_packages(),
     install_requires=[
         "requests", 
