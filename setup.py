096a4eda41c38d680e5e871c05924338c22d5741
PepeRedondoRubio <peperedondorubio@gmail.com>
2025-12-12 09:40:48 +0100

    feat: update version to 5.0.0 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pulpo",
-    version="4.10.8",
+    version="5.0.0",
     packages=find_packages(),
     install_requires=[
         "requests", 
