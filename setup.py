92b25e05919bf61722bc04423c2b4d36f6127e5f
PepeRedondoRubio <peperedondorubio@gmail.com>
2026-02-05 12:56:49 +0100

    Cierre de version para cambiar el authenticate (Sergio)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pulpo",
-    version="5.1.16",
+    version="5.2.0",
     packages=find_packages(),
     install_requires=[
         "requests", 
