dc73e01d2217128c969d86444e3a0f31516ae274
PepeRedondoRubio <peperedondorubio@gmail.com>
2026-02-05 15:07:27 +0100

    Validar propuesta en el api sincrono de erp_dolibarr

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pulpo",
-    version="5.2.0",
+    version="5.2.1",
     packages=find_packages(),
     install_requires=[
         "requests", 
