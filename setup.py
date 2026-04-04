84c3f7f293235ab3ea36b077b26590c0a81874fd
PepeRedondoRubio <peperedondorubio@gmail.com>
2026-04-04 16:42:53 +0200

    Bump version to 5.4.9 in setup.py and add __init__.py for auth module

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pulpo",
-    version="5.4.8",
+    version="5.4.9",
     packages=find_packages(),
     install_requires=[
         "requests",
